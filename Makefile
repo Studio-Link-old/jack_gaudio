515fc82713192b57702cd0e77fc242b9deef445c
Sebastian Reimers <info@miete-admin.de>
2015-01-11 01:10:26 +0100

    add some jack2 libs and complete jack_out.c

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 default: jack_gaudio_out
 
 jack_gaudio_out: alsa.c jack_out.c
-	gcc alsa.c jack_out.c -o jack_gaudio_out -l jack -l asound
+	gcc jack2/memops.c alsa.c jack_out.c -o jack_gaudio_out -l jack -l asound -l pthread -lm -I jack2 -I jack2/jack
 
 clean:
 	rm -f jack_gaudio_out
--- jack_out.c
+++ jack_out.c
@@ -5,9 +5,11 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <math.h>
 
 #include <jack/jack.h>
 #include <jack/ringbuffer.h>
+#include <memops.h>
 
 #include <alsa/asoundlib.h>
 #include "alsa.h"
@@ -31,7 +33,7 @@ jack_nframes_t nframes;
 const size_t sample_size = sizeof(jack_default_audio_sample_t);
 
 /* Synchronization between process thread and disk thread. */
-#define DEFAULT_RB_SIZE 9600		/* ringbuffer size in frames */
+#define DEFAULT_RB_SIZE 96000		/* ringbuffer size in frames */
 jack_ringbuffer_t *rb;
 pthread_mutex_t alsa_thread_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  data_ready = PTHREAD_COND_INITIALIZER;
@@ -58,33 +60,26 @@ static void jack_shutdown (void *arg)
 
 static void * alsa_thread (void *arg)
 {
+	nframes = 512;
 	jack_thread_info_t *info = (jack_thread_info_t *) arg;
-	static jack_nframes_t total_captured = 0;
-	jack_nframes_t samples_per_frame = info->channels;
+	jack_nframes_t samples_per_frame = info->channels * nframes;
 	size_t bytes_per_frame = samples_per_frame * sample_size;
 	void *framebuf = malloc (bytes_per_frame);
+	void *resamplebuf = malloc (samples_per_frame);
 
 	pthread_setcanceltype (PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 	pthread_mutex_lock (&alsa_thread_lock);
 
 	info->status = 0;
-
 	while (1) {
-
-		/* Write the data one frame at a time.  This is
-		 * 		 * inefficient, but makes things simpler. */
 		while (info->can_capture &&
 				(jack_ringbuffer_read_space (rb) >= bytes_per_frame)) {
 
 			jack_ringbuffer_read (rb, framebuf, bytes_per_frame);
-
-			//@TODO WRITE ALSA
-
-			if (++total_captured >= info->duration) {
-				printf ("disk thread finished\n");
-				goto done;
-			}
+			sample_move_d16_sS((char*)resamplebuf, framebuf, bytes_per_frame, 2, NULL);
+			audio_alsa_play_write(resamplebuf, nframes);
 		}
+	//		printf("buffer2: %d \n",jack_ringbuffer_read_space (rb));
 
 		/* wait until process() signals more data */
 		pthread_cond_wait (&data_ready, &alsa_thread_lock);
@@ -97,10 +92,10 @@ done:
 
 static void setup_alsa_thread (jack_thread_info_t *info)
 {
-	au.device = "hw:0,0";
+	au.device = "hw:1,0";
 	au.sample_rate = 48000;
 	au.channels = 2;
-	au.buffer = 480;
+	au.buffer = 500000;
 	audio_alsa_play_open(&au);
 	info->can_capture = 0;
 	pthread_create (&info->thread_id, NULL, alsa_thread, info);
@@ -119,16 +114,16 @@ static int process (jack_nframes_t nframes, void *arg)
 	for (chn = 0; chn < nports; chn++)
 		in[chn] = jack_port_get_buffer (ports[chn], nframes);
 
-	/* Sndfile requires interleaved data.  It is simpler here to
-	 * 	 * just queue interleaved samples to a single ringbuffer. */
 	for (i = 0; i < nframes; i++) {
 		for (chn = 0; chn < nports; chn++) {
-			if (jack_ringbuffer_write (rb, (void *) (in[chn]+i),
+
+			if (jack_ringbuffer_write (rb, (char *) (in[chn]+i),
 						sample_size)
 					< sample_size)
 				overruns++;
 		}
 	}
+
 
 	/* Tell the disk thread there is work to do.  If it is already
 	 * 	 * running, the lock will not be available.  We can't wait
@@ -139,7 +134,6 @@ static int process (jack_nframes_t nframes, void *arg)
 		pthread_cond_signal (&data_ready);
 		pthread_mutex_unlock (&alsa_thread_lock);
 	}
-
 	return 0;
 }
 
@@ -150,12 +144,48 @@ static void run_alsa_thread (jack_thread_info_t *info)
 	audio_alsa_play_close();
 	if (overruns > 0) {
 		fprintf (stderr,
-				"jackrec failed with %ld overruns.\n", overruns);
+				"jack_gaudio_out failed with %ld overruns.\n", overruns);
 		fprintf (stderr, " try a bigger buffer than -B %"
 				PRIu32 ".\n", info->rb_size);
 		info->status = EPIPE;
 	}
 }
+
+static void setup_ports (jack_thread_info_t *info)
+{
+	unsigned int i;
+	size_t in_size;
+
+	/* Allocate data structures that depend on the number of ports. */
+	ports = (jack_port_t **) malloc (sizeof (jack_port_t *) * nports);
+	in_size =  nports * sizeof (jack_default_audio_sample_t *);
+	in = (jack_default_audio_sample_t **) malloc (in_size);
+	rb = jack_ringbuffer_create (nports * sample_size * info->rb_size);
+
+
+	/* When JACK is running realtime, jack_activate() will have
+	 * called mlockall() to lock our pages into memory.  But, we
+	 * still need to touch any newly allocated pages before
+	 * process() starts using them.  Otherwise, a page fault could
+	 * create a delay that would force JACK to shut us down. */
+	memset(in, 0, in_size);
+	memset(rb->buf, 0, rb->size);
+
+	for (i = 0; i < nports; i++) {
+		char name[64];
+
+		sprintf (name, "input%d", i+1);
+
+		if ((ports[i] = jack_port_register (info->client, name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0)) == 0) {
+			fprintf (stderr, "cannot register input port \"%s\"!\n", name);
+			jack_client_close (info->client);
+			exit (1);
+		}
+	}
+
+	info->can_process = 1;		/* process() can start, now */
+}
+
 
 
 int main (int argc, char *argv[])
@@ -183,18 +213,10 @@ int main (int argc, char *argv[])
 		fprintf (stderr, "cannot activate client");
 	}
 
-	//Setup Ports
-	output_port0 = jack_port_register(client, "out0", 
-			JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-
-	output_port1 = jack_port_register(client, "out1", 
-			JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-
-	thread_info.can_process = 1;
+	setup_ports(&thread_info);
 
 	signal(SIGTERM, signal_handler);
 	signal(SIGINT, signal_handler);
-
 	run_alsa_thread (&thread_info);
 
 	jack_client_close (client);
